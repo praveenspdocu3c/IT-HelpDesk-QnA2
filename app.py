f55bc420a33492bf75a3bc225e97d9f749699f18
praveenspdocu3c <praveens@docu3c.com>
2024-09-11 20:34:13 +0530

    Update app.py

--- app.py
+++ app.py
@@ -12,10 +12,6 @@ from langchain.chains.combine_documents import create_stuff_documents_chain
 from langchain_core.prompts import PromptTemplate
 import pandas as pd
 from fpdf import FPDF
-from langchain import hub
-
-from langchain_core.output_parsers import StrOutputParser
-from langsmith import Client
 
 # Azure OpenAI settings
 api_key = "783973291a7c4a74a1120133309860c0"  
@@ -24,6 +20,7 @@ api_version = "2024-02-01"
 
 # Streamlit UI
 st.title("IT HelpDesk QnA")
+
 
 from langchain_community.document_transformers.openai_functions import (
     create_metadata_tagger,
@@ -106,19 +103,6 @@ if excel_files:
     user_problem = st.text_input("Enter the problem statement for resolution:", "")
 
     if st.button("Get Resolution"):
-
-        # Prepare LLM model for response generation
-        llm = AzureChatOpenAI(
-            api_version=api_version,
-            azure_endpoint=azure_endpoint,
-            openai_api_key=api_key,
-            model="gpt-4o-mini",
-            base_url=None,
-            azure_deployment="GPT-4o-mini"
-        )
-
-        llm.validate_base_url = False        
-        
         query = f"""What is the Resolution for the exact below mentioned Problem:\n {user_problem}\n\n 
                     Try to extract the resolution until any of this title arrives "NOTES" , "Short description:"\n
                     Note: Extract the exact answer from the input context and the answer of the problem will be part of the Resolution that can be found in Article body"""
@@ -130,6 +114,18 @@ if excel_files:
         reordering = LongContextReorder()
         reordered_docs = reordering.transform_documents(docs)
 
+        # Prepare LLM model for response generation
+        llm = AzureChatOpenAI(
+            api_version=api_version,
+            azure_endpoint=azure_endpoint,
+            openai_api_key=api_key,
+            model="gpt-4o-mini",
+            base_url=None,
+            azure_deployment="GPT-4o-mini"
+        )
+
+        llm.validate_base_url = False
+
         prompt_template = """
         Given these texts:
         -----
@@ -140,27 +136,7 @@ if excel_files:
         """
         prompt = PromptTemplate(template=prompt_template, input_variables=["context", "query"])
 
-        # Rewrite prompt section
-        client = Client(api_key=api_key)
-        templatesp = """Provide a better search query for \
-                    web search engine to answer the given question, end \
-                    the queries with ’**’. Question: \
-                    {x} Answer:"""
-
-        rewrite_prompt = PromptTemplate(template=templatesp)
-
-        # Pull the prompt from langsmith (replacement for hub)
-        rewrite_prompt = client.pull_prompt("langchain-ai/rewrite")
-
-        def _parse(text):
-            return text.strip('"').strip("**")
-                
-        # Create and invoke the rewriter chain
-        rewriter_chain = rewrite_prompt | llm | StrOutputParser() | _parse
-        input_data = {"messages": [{"role": "user", "content": user_problem}]}
-        rewritten_query = rewriter_chain.invoke(input_data)
-
-        
+        # Create and invoke the chain:
         chain = create_stuff_documents_chain(llm, prompt)
         response = chain.invoke({"context": reordered_docs, "query": query})
         
